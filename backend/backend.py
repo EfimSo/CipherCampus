f43237f36ff525999f0c6f6b9c2eb038107734d4
Efim Sokolov <efim@bu.edu>
2025-05-05 21:22:02 -0400

    Oopsie I commented out the proof checking

--- backend/backend.py
+++ backend/backend.py
@@ -90,9 +90,9 @@ def write_review():
                 vk = 3
             
         
-        # proof = arg_dict["proof"]
-        # if not proof or not verify_proof(proof, vk):
-        #     return jsonify({'error': "Proof Verification Failed"}), 500
+        proof = arg_dict["proof"]
+        if not proof or not verify_proof(proof, vk):
+            return jsonify({'error': "Proof Verification Failed"}), 500
         
         try: arg_dict["rating"] = float(arg_dict["rating"]) 
         except: arg_dict["rating"] = 1.0
